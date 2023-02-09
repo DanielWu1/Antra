246768409e4c3a36115ec6a132b07fc242461cef
DanielWu1 <Tony153265964@gmail.com>
2023-02-09 01:53:51 -0500

    done

--- AngularAssinment2/AngularAssinment2/src/app/list/list.component.ts
+++ AngularAssinment2/AngularAssinment2/src/app/list/list.component.ts
@@ -1,56 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { EachList } from '../interfaces';
+import { ListServiceService } from '../listService/list-service.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent {
-  List: EachList[] =[
-    {
-      name: 'Item 1',
-      child: [
-        {
-          name: 'Item 1.1',
-          child: [
-            { name: 'Item 1.1.1', child: [] },
-            { name: 'Item 1.1.2', child: [] },
-            {
-              name: 'Item 1.1.3',
-              child: [{ name: 'Item 1.1.3.1', child: [] }],
-            },
-            {
-              name: 'Item 1.1.4',
-              child: [
-                { name: 'Item 1.1.4.1', child: [] },
-                { name: 'Item 1.1.4.2', child: [] },
-              ],
-            },
-          ],
-        },
-        {
-          name: 'Item 1.2',
-          child: [
-            { name: 'Item 1.2.1', child: [] },
-            {
-              name: 'Item 1.2.2',
-              child: [{ name: 'Item 1.2.2.1', child: [] }],
-            },
-            {
-              name: 'Item 1.2.3',
-              child: [
-                { name: 'Item 1.2.3.1', child: [] },
-                {
-                  name: 'Item 1.2.3.2',
-                  child: [{ name: 'Item 1.2.3.2.1', child: [] }],
-                },
-                { name: 'Item 1.2.3.3', child: [] },
-              ],
-            },
-          ],
-        },
-      ],
-    },
-  ];
+export class ListComponent implements OnInit {
+  // List = new ListServiceService().List;
+  List !: EachList[];
+  constructor (public ListServiceService: ListServiceService){}
+  ngOnInit(): void{
+    this.List = this.ListServiceService.List;
+  }
 }
